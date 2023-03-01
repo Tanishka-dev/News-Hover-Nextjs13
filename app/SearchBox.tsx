1ea06127e06ba04956fdf369590747a9c1ae73c6
Tanishka Yadav <tanishka.yadav@miqdigital.com>
2023-03-02 00:13:52 +0530

    Search box added

--- app/SearchBox.tsx
+++ app/SearchBox.tsx
@@ -1,13 +1,25 @@
-'use client'
+"use client";
+
+import { useState } from "react";
 
 function SearchBox() {
+  const [input, setInput] = useState("");
   return (
-    <form className='max-w-6xl mx-auto flex jsutify-be
-    items-center'>
-        <input type="text" placeholder='Search Keywords...' className='w-full h-14 rounded-sm placeholder-gray-500 text-gray-500 outline-none bg-transparent dark:text-orange-400'/>
-        <button type="submit">Search</button>
+    <form
+      className="max-w-6xl mx-auto flex jsutify-be
+    items-center"
+    >
+      <input
+        type="text"
+        placeholder="Search Keywords..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 text-gray-500 outline-none bg-transparent dark:text-orange-400"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit" disabled={!input}>
+        Search
+      </button>
     </form>
-  )
+  );
 }
 
-export default SearchBox
+export default SearchBox;
